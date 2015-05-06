ec4e7db61058e594b148bde9e582140ac2d76965
OOPer <nagata@shlab.jp>
2015-05-06 13:33:16 +0900

    initial

---
+++ QRCodeGenerator/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  QRCodeGenerator
+//
+//  Created by OOPer in cooperation with shlab.jp, on 2015/5/6.
+//  Copyright (c) 2015 OOPer. All rights reserved. See LICENSE.txt .
+//
+
+import UIKit
+import AssetsLibrary
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        textField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        let text = NSUserDefaults.standardUserDefaults().stringForKey("text")
+            ?? "http://oopers.com"
+        textField.text = text
+        var size = imageView.frame.size
+        let image = QRCodeImage(text, size: size)
+        imageView.image = image
+    }
+    
+    @IBAction func uploadAndSave(AnyObject) {
+        let text = textField.text ?? ""
+        var size = imageView.frame.size
+        let image = QRCodeImage(text, size: size)
+        imageView.image = image
+        //
+        NSUserDefaults.standardUserDefaults().setValue(text, forKey: "text")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        //
+        ALAssetsLibrary().writeImageToSavedPhotosAlbum(image?.CGImage, orientation: ALAssetOrientation.Up, completionBlock: nil)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    func QRCodeImage(message: String, size: CGSize) -> UIImage? {
+        let data: NSData = message.dataUsingEncoding(NSUTF8StringEncoding)!
+        let params = [
+            "inputMessage": data,
+            "inputCorrectionLevel": "H"
+        ]
+        var qrEncoder = CIFilter(name: "CIQRCodeGenerator", withInputParameters: params)
+        let ciImage: CIImage = qrEncoder.outputImage
+        var image = UIImage(CIImage: ciImage)
+        //
+        UIGraphicsBeginImageContext(size);
+        let context = UIGraphicsGetCurrentContext();
+        CGContextSetInterpolationQuality(context, kCGInterpolationNone)//
+        image!.drawInRect(CGRect(origin: CGPoint(), size: size))
+        image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return image
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# QRCodeGenerator
+A simple example of QR Code generator using CIFilter.
+
+Created by OOPer in cooperation with shlab.jp, on 2015/5/6.
+
+Inspired by this article
+ <http://qiita.com/hkato193/items/c36a940c2929a124e416>
+.
+
+===========================================================================
+BUILD REQUIREMENTS:
+
+Xcode 6.3, iOS SDK 8.3
+
+===========================================================================
