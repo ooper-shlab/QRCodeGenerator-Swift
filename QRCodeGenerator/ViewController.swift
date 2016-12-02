6a90b96fb12e5738ea45a04810e7e4986c2c76b7
OOPer <nagata@shlab.jp>
2016-12-02 21:40:33 +0900

    Adapted to Swift 3

--- QRCodeGenerator/ViewController.swift
+++ QRCodeGenerator/ViewController.swift
@@ -3,7 +3,7 @@
 //  QRCodeGenerator
 //
 //  Created by OOPer in cooperation with shlab.jp, on 2015/5/6.
-//  Copyright (c) 2015 OOPer. All rights reserved. See LICENSE.txt .
+//  Copyright (c) 2015-2016 OOPer. All rights reserved. See LICENSE.txt .
 //
 
 import UIKit
@@ -24,8 +24,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    override func viewWillAppear(animated: Bool) {
-        let text = NSUserDefaults.standardUserDefaults().stringForKey("text")
+    override func viewWillAppear(_ animated: Bool) {
+        let text = UserDefaults.standard.string(forKey: "text")
             ?? "http://oopers.com"
         textField.text = text
         let size = imageView.frame.size
@@ -39,32 +39,32 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let image = QRCodeImage(text, size: size)
         imageView.image = image
         //
-        NSUserDefaults.standardUserDefaults().setValue(text, forKey: "text")
-        NSUserDefaults.standardUserDefaults().synchronize()
+        UserDefaults.standard.setValue(text, forKey: "text")
+        UserDefaults.standard.synchronize()
         //
-        ALAssetsLibrary().writeImageToSavedPhotosAlbum(image?.CGImage, orientation: ALAssetOrientation.Up, completionBlock: nil)
+        ALAssetsLibrary().writeImage(toSavedPhotosAlbum: image?.cgImage, orientation: ALAssetOrientation.up, completionBlock: nil)
     }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
 
-    func QRCodeImage(message: String, size: CGSize) -> UIImage? {
-        let data: NSData = message.dataUsingEncoding(NSUTF8StringEncoding)!
+    func QRCodeImage(_ message: String, size: CGSize) -> UIImage? {
+        let data: Data = message.data(using: String.Encoding.utf8)!
         let params = [
             "inputMessage": data,
             "inputCorrectionLevel": "H"
-        ]
+        ] as [String : Any]
         let qrEncoder = CIFilter(name: "CIQRCodeGenerator", withInputParameters: params)
         let ciImage: CIImage = qrEncoder!.outputImage!
-        var image = UIImage(CIImage: ciImage)
+        var image = UIImage(ciImage: ciImage)
         //
         UIGraphicsBeginImageContext(size);
         let context = UIGraphicsGetCurrentContext();
-        CGContextSetInterpolationQuality(context, CGInterpolationQuality.None)//
-        image.drawInRect(CGRect(origin: CGPoint(), size: size))
-        image = UIGraphicsGetImageFromCurrentImageContext();
+        context!.interpolationQuality = CGInterpolationQuality.none//
+        image.draw(in: CGRect(origin: CGPoint(), size: size))
+        image = UIGraphicsGetImageFromCurrentImageContext()!;
         UIGraphicsEndImageContext();
         return image
     }
